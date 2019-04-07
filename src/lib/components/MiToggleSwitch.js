e5ccc5c76f9f9f2700b6ed8305b360557c5dd48d
tradk <tradk1995@gmail.com>
2019-04-07 16:55:13 +0700

    Handle event, logic render MiToggleSwitch

---
+++ src/lib/components/MiToggleSwitch.js
@@ -0,0 +1,86 @@
+//@flow
+import * as React from 'react';
+import { StyledMiToggleSwitch } from './style';
+
+// flow define prop-types
+type Props = {
+  isEnable?: boolean,
+  isStateChange?: boolean,
+  onClick?: Function,
+  onStateChanged?: Function,
+  width?: number,
+  height?: number,
+  spacing?: number,
+  activeColor?: string,
+  deactiveColor?: string,
+  ballColor?: string,
+  className?: string,
+};
+
+// flow define state
+type State = {
+  enabled: boolean,
+};
+
+// default props
+const defaultProps = {
+  isEnable: true,
+  isStateChange: true,
+  height: 32,
+  spacing: 2,
+  ballColor: '#ffffff',
+  activeColor: '#62c28e',
+  deactiveColor: '#cccccc'
+};
+
+class MiToggleSwitch extends React.Component<Props, State> {
+  // define default props
+  static defaultProps = defaultProps;
+  
+  // define state
+  state = {
+    enabled: this.props.isEnable,
+  };
+
+  // Update state when props change
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isEnable !== this.state.enabled) {
+      this.setState({ enabled: nextProps.isEnable });
+    }
+  }
+
+  // check variable is function
+  isFunction = type => typeof type === 'function';
+
+  // event when click togglw switch
+  toggleSwitch = evt => {
+    evt.persist();
+    evt.preventDefault();
+    const { onClick, isStateChange, onStateChanged } = this.props;
+    if (isStateChange) {
+      this.setState({ enabled: !this.state.enabled }, () => {
+        const state = this.state;
+        const switchEvent = Object.assign(evt, { SWITCH_STATE: state });
+        this.isFunction(onClick) && onClick(switchEvent);
+        this.isFunction(onStateChanged) && onStateChanged(state);
+      });
+    } else {
+      this.contextisFunction(onClick) && onClick();
+    }
+  }
+
+  // render component
+  render() {
+    const { enabled } = this.state;
+    const { className, onClick, onStateChanged, isEnable, isStateChange, ...restProps } = this.props;
+    return (
+      <StyledMiToggleSwitch
+        className={className}
+        onClick={this.toggleSwitch}
+        {...restProps}
+        enabled={enabled} />
+    );
+  }
+}
+
+export default MiToggleSwitch;
