63b1ea8666e394dbb0f8b99f56595d3f01dc1aba
tradk <tradk1995@gmail.com>
2019-04-07 17:04:15 +0700

    Update index.js, export MiToggleSwitch component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+import App from './example/App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
