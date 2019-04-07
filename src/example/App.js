b679da7c9a7210cd63e3e44c280dcb6c28b86eea
tradk <tradk1995@gmail.com>
2019-04-07 16:54:01 +0700

    Add example folder

---
+++ src/example/App.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react';
+import { MiToggleSwitch } from '../lib';
+
+const App = () => (
+  <div style={{ width: 900, margin: "15px auto" }}>
+    <MiToggleSwitch />
+  </div>
+)
+
+export default App;
